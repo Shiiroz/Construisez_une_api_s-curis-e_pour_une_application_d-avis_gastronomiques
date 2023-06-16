98aad1d3df691823f8d2e52d08c10d6467dadc0b
Ryan Mortuaire <ryanmortuaire@gmail.com>
2023-06-16 20:47:23 +0200

    2eme

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
+
 mongoose.connect('mongodb+srv://projet6:ryan@atlascluster.srnuj7i.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res) => {
+  res.json({message: 'recu'})
+})
 
 module.exports = app;
